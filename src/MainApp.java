8f70a03fb42bddd0e3666b35dd49471a3caf5d7b
user <onikaankaymaz@gmail.com>
2022-11-18 19:55:56 +0300

    Kaan Kaymaz - Homework 5 - 18.11.2022

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,5 +1,10 @@
 public class MainApp {
     public static void main(String[] args) {
-
+        Wall wall1=new Wall(5,4);
+        System.out.println("area= " + wall1.getArea());
+        wall1.setHeight(-1.5);
+        System.out.println("Width = " + wall1.getWidth());
+        System.out.println("Height = " + wall1.getHeight());
+        System.out.println("Area = " + wall1.getArea());
     }
 }
