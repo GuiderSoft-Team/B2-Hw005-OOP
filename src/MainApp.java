02e835a425fe7a9f6f550483dae8324e8679264b
burcuturanugur <107644861+burcuturanugur@users.noreply.github.com>
2022-11-19 09:44:56 +0300

    Homework5 is done

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,5 +1,13 @@
 public class MainApp {
     public static void main(String[] args) {
 
+        Wall wall=new Wall(5,4);
+        System.out.println("Area= " +wall.getArea());
+        wall.setHeight(-1.5);
+        System.out.println("Width= " +wall.getWidth());
+        System.out.println("Height= " +wall.getHeight());
+        System.out.println("Area= " +wall.getArea());
+
     }
+
 }
